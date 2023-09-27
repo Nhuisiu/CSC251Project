c7fdb497f76bcb8f58c54025f448c4282ff6159c
Nhuisiu <Nhuisiu@gmail.com>
2023-09-27 18:19:48 -0400

    Added documentation comments
    Added documentation comments

--- CSC251Project_Nhui_Siu.java
+++ CSC251Project_Nhui_Siu.java
@@ -1,20 +1,4 @@
 public class CSC251Project_Nhui_Siu {
     public static void main(String[] args) {
-        InsurancePolicy policy = new InsurancePolicy();
-        policy.setPolicyNumber(0);
-        policy.setProviderName("");
-        policy.setFirstName("");
-        policy.setLastName("");
-        policy.setAge(0);
-        policy.setSmokingStatus("");
-        policy.setHeight(0);
-        policy.setWeight(0);
-
-        double bmi = policy.bmi(policy.getWeight(), policy.getHeight());
-        double price = policy.priceOfInsurancePolicy(policy.getAge(), policy.getSmokingStatus(), bmi);
-        policy.displayInformation(bmi, price);
-        System.out.print(bmi);
-
-
     }
 }
