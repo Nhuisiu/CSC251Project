d38a4b6255f348ddb6de26f8f9d277ad93c31386
Nhuisiu <Nhuisiu@gmail.com>
2023-10-28 22:33:11 -0400

    Static
    Added static field to track number of Policy objects

--- Policy.java
+++ Policy.java
@@ -2,12 +2,15 @@
 
 public class Policy {
 
+    private static int numOfPolicyMade;
     private int policyNumber;
     private String providerName;
+    
 
     public Policy(int policyNumber, String providerName, PolicyHolder policyHolder) {
         this.policyNumber = policyNumber;
         this.providerName = providerName;
+        numOfPolicyMade++;
     }
 
     public int getPolicyNumber() {
