c2704617b2d5a073f3eb7468f27aea14d6860da1
Nhuisiu <Nhuisiu@gmail.com>
2023-10-28 22:27:41 -0400

    Added toString method
    Added toString method

--- Policy.java
+++ Policy.java
@@ -1,5 +1,35 @@
 // Creating Insurance Policy class for getter and setter
 
 public class Policy {
-        
+
+    private int policyNumber;
+    private String providerName;
+
+    public Policy(int policyNumber, String providerName, PolicyHolder policyHolder) {
+        this.policyNumber = policyNumber;
+        this.providerName = providerName;
+    }
+
+    public int getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public void setPolicyNumber(int policyNumber) {
+        this.policyNumber = policyNumber;
+    }
+
+    public String getProviderName() {
+        return providerName;
+    }
+
+    public void setProviderName(String providerName) {
+        this.providerName = providerName;
+    }
+
+    //convert to String method
+    public String toString() {
+        return String.format("Policy Number: " + policyNumber +
+                "\nProvider Name: " + providerName);
+    }
+
 }
